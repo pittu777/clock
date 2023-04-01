b6e5407985769e2bb5a05537fc238b75dd1a4fa9
pittu777 <you@example.com>
2023-04-01 16:05:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+setInterval(displayClock, 500);
+function displayClock(){
+    var time=new Date();
+    var hrs=time.getHours();
+    var min=time.getMinutes();
+    var sec=time.getSeconds();
+    var pittu='AM';
+    if(hrs>12){
+        pittu='PM';
+    }
+    if(hrs>12){
+        hrs=hrs-12;
+    }
+    if(hrs===0){
+        hrs=12;
+    }
+    if(hrs<10){
+        hrs='0'+hrs;
+    }
+    if(min<10){
+        min='0'+min;
+    }
+    if(sec<10){
+        sec='0'+sec;
+    }
+    document.getElementById('clock').innerHTML=hrs+':'+min+':'+sec+' '+ pittu;
+    document.getElementById('clock').style.color='steelblue';
+}
+
+
+
